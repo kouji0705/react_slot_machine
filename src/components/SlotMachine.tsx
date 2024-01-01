ca64f19fcffebbe6436570c293219f313d9b56ec
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-01-01 15:22:33 +0900

    add: test

--- src/components/SlotMachine.tsx
+++ src/components/SlotMachine.tsx
@@ -4,20 +4,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import {BET_COINS, INITIAL_COINS, SYMBOLS} from "./const"; // SymbolTileのインポート
 import {SymbolTile} from "./type";
-
-const spinReels = (
-  currentReels: SymbolTile[],
-  symbols: SymbolTile[],
-  betCoins: number
-): {newReels: SymbolTile[]; win: boolean; points: number} => {
-  const newReels = currentReels.map(
-    () => symbols[Math.floor(Math.random() * symbols.length)]
-  );
-  const win = new Set(newReels.map((tile) => tile.id)).size === 1;
-  const points = win ? newReels[0].point : 0;
-
-  return {newReels, win, points};
-};
+import {spinReels} from "./func";
 
 export const SlotMachine: React.FC = () => {
   const [coins, setCoins] = useState(INITIAL_COINS);
