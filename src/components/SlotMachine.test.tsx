8b1143bc0335a2eef031751cc6f3fb32d4061142
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-01-01 13:40:31 +0900

    add: test

--- src/components/SlotMachine.test.tsx
+++ src/components/SlotMachine.test.tsx
@@ -20,3 +20,29 @@ describe("SlotMachine コンポーネント", () => {
     expect(updatedReels).not.toEqual(initialReels);
   });
 });
+
+describe("コインの消費", () => {
+  test("スピンボタンをクリックするとコインが3枚消費される", () => {
+    render(<SlotMachine />);
+    fireEvent.click(screen.getByRole("button", { name: "スピン" }));
+    expect(screen.getByText("Coinの枚数：97枚")).toBeInTheDocument();
+  });
+  test("コインが3枚以上あるときはスピンボタンをクリックできる", () => {
+    render(<SlotMachine />);
+    expect(screen.getByRole("button", { name: "スピン" })).toBeEnabled();
+  });
+  test("コインが0枚になるとスピンボタンは無効になる", () => {
+    render(<SlotMachine />);
+    const spinButton = screen.getByRole("button", {
+      name: "スピン",
+    }) as HTMLButtonElement;
+
+    // コインを0枚にするまでボタンをクリックする
+    while (!spinButton.disabled) {
+      fireEvent.click(spinButton);
+    }
+
+    // ボタンが無効化されていることを検証
+    expect(spinButton).toBeDisabled();
+  });
+});
