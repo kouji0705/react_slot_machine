de4e8cdd8286f3353637976174b6977ea4672939
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-01-02 14:09:46 +0900

    add: Reel styled

--- src/features/slotMachine/components/Reel.tsx
+++ src/features/slotMachine/components/Reel.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import styled from "@emotion/styled";
 
 type ReelProps = {
   symbol: string;
 };
 
-export const Reel: React.FC<ReelProps> = ({ symbol }) => {
-  return (
-    <Box
-      sx={{
-        fontSize: "2rem",
-        padding: "10px",
-        border: "1px solid #ccc",
-        borderRadius: "4px",
-      }}
-    >
-      {symbol}
-    </Box>
-  );
+export const Reel: React.FC<ReelProps> = ({symbol}) => {
+  return <RootContainer>{symbol}</RootContainer>;
 };
+
+const RootContainer = styled(Box)(() => ({
+  fontSize: "2rem",
+  padding: "10px",
+  border: "1px solid #ccc",
+  borderRadius: "4px",
+}));
